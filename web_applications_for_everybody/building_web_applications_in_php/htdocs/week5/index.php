e29967fbd0c8072e2975d4e3d5fed38ddf7d71b1
Kyle <kylewtho@gmail.com>
2022-01-17 13:05:30 +0800

    Uploaded files

---
+++ web_applications_for_everybody/building_web_applications_in_php/htdocs/week5/index.php
@@ -0,0 +1,21 @@
+<html>
+   <head>
+      <title>Kyle PHP</title>
+   </head>
+
+   <body>
+      <h1>Kyle PHP</h1>
+      <p>
+         <pre>
+    **   **
+    **  **
+    *****
+    **  **
+    **   **
+         </pre>
+      </p>
+      <?php
+         print hash('sha256', 'Kyle');
+      ?>
+   </body>
+</html>
